3be0741c1107ddd8f833a2dc4b7485d4dbaa80b7
David Gaspar <davidgaspar.dev@gmail.com>
2018-10-29 21:18:38 -0300

    Init Realm

--- app/src/main/java/com/dev/davidgaspar/realm/MainActivity.java
+++ app/src/main/java/com/dev/davidgaspar/realm/MainActivity.java
@@ -2,12 +2,23 @@ package com.dev.davidgaspar.realm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = MainActivity.class.getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Realm.init(this);
+        Realm realm = Realm.getDefaultInstance();
+
+        Log.w(LOG_TAG, "Realm | " + realm);
+
     }
 }
